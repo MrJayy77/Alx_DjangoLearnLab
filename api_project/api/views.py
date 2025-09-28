88168b3bb2e7d54fa452696ba63d280ad18f35ee
Abraham Kwesi James <abrahamkwesijames@gmail.com>
2025-09-28 16:12:07 +0000

    Yayy

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -6,3 +6,12 @@ from .serializers import BookSerializer
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+from rest_framework import viewsets
+from .models import Book
+from .serializers import BookSerializer
+
+# ViewSet to handle all CRUD operations (Create, Read, Update, Delete)
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
