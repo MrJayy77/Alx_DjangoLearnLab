a5dfc76574f9cab7a096fedea4e3958764efd5e5
Abraham Kwesi James <abrahamkwesijames@gmail.com>
2025-09-28 23:45:01 +0000

    Yayy

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
+from django_filters import rest_framework as filters
 
 # List all books with filtering, searching, ordering
 class BookListView(generics.ListAPIView):
